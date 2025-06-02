ffe5fed6c5ee83dd1a53fd776b42ea7c760ca271
monxchrome <inacheat@gmail.com>
2025-06-02 16:13:55 +0200

    CORS

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,18 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('API Klever Gurme')
     .setDescription('Klever Gurme restaurant')
-    .setVersion('5.13.19-alpha.5')
+    .setVersion('5.13.20-alpha.6')
     .addTag('restaurant')
     .build();
   const document = SwaggerModule.createDocument(app, config);
